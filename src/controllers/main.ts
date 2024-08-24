f2e426cf156df8610a87ca3b43631cb56b8a04f3
Nicolás Navarro <nicohnavarro@gmail.com>
2024-08-24 23:32:41 +0200

    📝 Add validation

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { Controller } from "../decorators/controller";
 import { Route } from "../decorators/route";
+import Joi from "joi";
+import { Validate } from "../decorators/validate";
+
+const postHealthCheckValidation = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email()
+});
 
 @Controller()
 class MainController {
@@ -8,6 +15,13 @@ class MainController {
   getHealthCheck(req: Request, res: Response, next: NextFunction) {
     console.info("🩺 HealthCheck called successfully!");
     return res.status(200).json({ status: "✅ Works fine!" });
+  }
+
+  @Route("post", "/healthcheck")
+  @Validate(postHealthCheckValidation)
+  postHealthCheck(req: Request, res: Response, next: NextFunction) {
+    console.info("🩺 HealthCheck called successfully!");
+    return res.status(200).json({ ...req.body });
   }
 }
 
---
+++ src/decorators/validate.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import Joi from "joi";
+
+export function Validate(schema: Joi.ObjectSchema) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) {
+      try {
+        await schema.validateAsync(req.body);
+      } catch (error) {
+        console.error(`🚨 ${(error as Error).message}`);
+        return res.status(422).json(error);
+      }
+      return originalMethod.call(this, req, res, next);
+    };
+    return descriptor;
+  };
+}
